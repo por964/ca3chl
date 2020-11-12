8888e54cd96e279e60f5c062ce239275890b9c61
Claes <claesvonh@gmail.com>
2020-11-12 16:18:33 +0100

    test db changed in Travis file

--- src/main/java/jokefetcher/JokeFetcher.java
+++ src/main/java/jokefetcher/JokeFetcher.java
@@ -55,20 +55,5 @@ public static String responseParallel (ExecutorService threadPool, final Gson gs
        
     }
 
-
-//    
-//    public static void main(String[] args) throws IOException {
-//
-//
-//        String chuck = HttpUtils.fetchData("https://api.chucknorris.io/jokes/random");
-//        String dad = HttpUtils.fetchData("https://icanhazdadjoke.com");
-//        
-//        System.out.println("JSON fetched from chucknorris:");
-//        System.out.println(chuck);
-//        System.out.println("JSON fetched from dadjokes:");
-//        System.out.println(dad);
-//        
-//       
-//    }
 }    
 
